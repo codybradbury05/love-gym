bad9d6bea6b20eab526a059c04bb5f2483a53d55
codybradbury05 <>
2023-12-14 21:09:04 +0000

    Created inital variables and question/answers list

---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let avaiableQuestions = [];
+
+let questions = [
+    {
+        question: "Which exercise best trains your chest?",
+        choice1: "Bench Press",
+        choice2: "Squat",
+        choice3: "Deadlift",
+        choice4: "Romanian Deadlift",
+        answer: 1
+    },
+    {
+        question: "Which exercise best trains your back?",
+        choice1: "Bench Press",
+        choice2: "Squat",
+        choice3: "Deadlift",
+        choice4: "Romanian Deadlift",
+        answer: 3
+    },
+    {
+        question: "Which exercise best trains your legs?",
+        choice1: "Bench Press",
+        choice2: "Squat",
+        choice3: "Deadlift",
+        choice4: "Romanian Deadlift",
+        answer: 2
+    },
+    {
+        question: "How much protein should we consume to gain muscle?",
+        choice1: "3g/KG Bodyweight",
+        choice2: "0.75g/KG Bodyweight",
+        choice3: "60g",
+        choice4: "100g",
+        answer: 2
+    },
+    {
+        question: "What does creatine do?",
+        choice1: "Helps reproduction of cells",
+        choice2: "Enhances protein stores",
+        choice3: "Supplies energy to your muscles",
+        choice4: "Aids joint health",
+        answer: 3
+    }
+]
